a49c5fe04183a44b18b0f93744a29d0efd9cf352
Sibi Chakravarthy Ramesh <sibiitdeveloper@gmail.com>
2021-07-25 00:19:28 +0530

    fix: no ptr

--- kmsgparser/messages.go
+++ kmsgparser/messages.go
@@ -7,10 +7,10 @@ import (
 
 type Messages []*Message
 
-func (msgs *Messages) String() string {
+func (msgs Messages) String() string {
 
 	msg := ""
-	for _, m := range *msgs {
+	for _, m := range msgs {
 		msg += fmt.Sprintf("(%d) - %s: %s", m.SequenceNumber, m.Timestamp.Format(time.RFC3339Nano), m.Message)
 	}
 
